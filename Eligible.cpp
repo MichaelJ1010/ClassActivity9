863cf7f3ea144e62c0314edd7673b4f1b2995063
Michael J <30031129@cc.peralta.edu>
2024-09-15 17:29:00 -0700

    added eligibility to work or not

--- Eligible.cpp
+++ Eligible.cpp
@@ -6,11 +6,15 @@ int main(){
     bool eligible;
     string isUSCitizen;
     int workExperience;
-    cout << "Are you a U.S citizen?" << endl;
+    cout << "Are you a U.S citizen?" <<endl;
     cin >> isUSCitizen;
     cout << "How many years of work experience do you have?" << endl;
     cin >> workExperience;
-
-
+    eligible = (isUSCitizen == "yes") || (workExperience >= 2);
+    if(eligible){
+        cout << "You are eligible to work.";
+    }else{
+        cout << "You are not eligible to work.";
+    }
     return 0;
 }
