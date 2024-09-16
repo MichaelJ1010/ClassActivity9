0c2311d373a96e389e87c220a02d35daceebcfd0
Michael J <30031129@cc.peralta.edu>
2024-09-15 17:23:57 -0700

    added quentionaire to determine eligibility

--- Eligible.cpp
+++ Eligible.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){
+    bool eligible;
+    string isUSCitizen;
+    int workExperience;
+    cout << "Are you a U.S citizen?" << endl;
+    cin >> isUSCitizen;
+    cout << "How many years of work experience do you have?" << endl;
+    cin >> workExperience;
+
 
     return 0;
 }
